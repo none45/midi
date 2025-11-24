d1e78502eb9240fbd85a90724ad8b334d544f1f7
none45 <dominiksdrobisevsmomosja@gmail.com>
2025-11-24 21:28:50 +0200

    Update merge_midis.py

--- .github/scripts/merge_midis.py
+++ .github/scripts/merge_midis.py
@@ -1,4 +1,4 @@
-from mido import MidiFile, MidiTrack
+from mido import MidiFile, MidiTrack, MetaMessage
 
 playlist_path = "midis/playlist.txt"
 output_path = "midis/playlist.mid"
@@ -7,14 +7,24 @@ with open(playlist_path, "r") as f:
     midi_paths = [line.strip() for line in f.readlines() if line.strip()]
 
 combined = MidiFile()
-track = MidiTrack()
-combined.tracks.append(track)
+ticks_per_beat = combined.ticks_per_beat
+
+master_track = MidiTrack()
+combined.tracks.append(master_track)
+
+master_track.append(MetaMessage('set_tempo', tempo=500000, time=0))
 
 for path in midi_paths:
     m = MidiFile(path)
-    for msg in m.tracks[0]:
-        track.append(msg)
+    
+    master_track.append(MetaMessage('marker', text=f'Start {path}', time=0))
+
+    for t in m.tracks:
+        new_track = MidiTrack()
+        combined.tracks.append(new_track)
+
+        for msg in t:
+            new_track.append(msg.copy(time=msg.time))
 
 combined.save(output_path)
-
 print("Merged MIDI saved:", output_path)
